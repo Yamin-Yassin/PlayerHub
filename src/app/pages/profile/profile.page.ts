1aabfa60581662504e699b99b83e318aadc385b0
Yamin Yassin <yamin2008@live.com>
2021-08-03 20:13:57 +0100

    added way to change segment page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  selectedSegment:string;
   username:string;
   constructor() { }
 
   ngOnInit() {
     this.username="yaminyassin"
+  }
+
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
   }
 
 }
