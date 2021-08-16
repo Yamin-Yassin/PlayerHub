b92c67a37333ba5d55188defe39a5f33ee573e50
Yamin Yassin <x210621@bcpcorp.net>
2021-08-16 12:28:37 +0100

    review component  done!

--- src/app/pages/game/game.module.ts
+++ src/app/pages/game/game.module.ts
@@ -7,9 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { GamePageRoutingModule } from './game-routing.module';
 
 import { GamePage } from './game.page';
+import { ReviewGameComponent } from '@components/review-game/review-game.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, GamePageRoutingModule],
-  declarations: [GamePage],
+  declarations: [GamePage, ReviewGameComponent],
 })
 export class GamePageModule {}
