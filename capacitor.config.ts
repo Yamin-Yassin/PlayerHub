643f6622ddd39c15a68ae27c41b45f3c865632be
Yamin Yassin <x210621@bcpcorp.net>
2021-08-23 00:11:20 +0100

    android done migrating to cap3; added push-notif

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,16 @@
-import {CapacitorConfig } from '@capacitor/cli'
+/// <reference types="@capacitor/push-notifications" />
 
-const config:CapacitorConfig = {
-    appId: "io.ionic.starter",
-    appName: "PlayerHub",
-    webDir: "www",
-    plugins: {
-        SpalshScreen:{
-            launchShowDuration:0,
-        }
-    }
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PlayerHub',
+  webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+  },
 };
 
 export default config;
